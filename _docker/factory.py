91c48bd24f37495969e640b8c3667a91bfa02721
David Angarita <davidangarita.sistemas@gmail.com>
2025-09-17 18:56:06 -0500

    Create typedDict DockerConfig

--- _docker/factory.py
+++ _docker/factory.py
@@ -1,11 +1,23 @@
 """Create, build and run docker containers with test commands."""
 
+from __future__ import annotations
+
 import sys
+from typing import TypedDict
 
 import docker
 from docker.errors import DockerException
 
 # ruff: noqa: T201 allow print in CLI
+
+
+class DockerConfig(TypedDict):
+    """Type a docker config."""
+
+    name: str
+    os_name: str
+    commands: list[str]
+
 
 try:
     client = docker.from_env()
